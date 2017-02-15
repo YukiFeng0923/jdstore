2f09c0f192c785d2407885afe45d838fe9612a3c
YukiFeng0923 <fcx0923@163.com>
2017-02-16 00:10:23 +0800

    finish slide

--- app/assets/welcome.js
+++ app/assets/welcome.js
@@ -1,6 +1,31 @@
 // Carousel Auto-Cycle
-  $(document).ready(function() {
-    $('.carousel').carousel({
+$(document).ready(function() {
+    $('#carousel1').carousel({
       interval: 6000
     })
   });
+
+
+$(document).ready(function() {
+      $('#carousel2').carousel({
+        interval: 6000
+      })
+    });
+
+$(document).ready(function() {
+          $('#carousel3').carousel({
+            interval: 6000
+          })
+        });
+
+$(document).ready(function() {
+              $('#carousel4').carousel({
+                interval: 6000
+              })
+            });
+
+$(document).ready(function() {
+                  $('#carousel5').carousel({
+                    interval: 6000
+                  })
+                });
